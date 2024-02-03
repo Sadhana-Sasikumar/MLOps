b0baf76237beed56869694ac59c9aac131190a3a
Sadhana Sasikumar <sadhanasasikumar12@gmail.comm>
2024-02-03 18:12:16 +0530

    structure updated

--- template.py
+++ template.py
@@ -1,31 +1,23 @@
 import os
 from pathlib import Path
-import logging
 
-list_of_files =[
-    ".github/workflows/.gitkeep", #when the final file name is not decided we can create a .gitkeep file
-    "src/__init__.py",
-    "src/components/data_ingestion.py",
-    "src/components/data_transformation.py",
-    "src/components/model_trainer.py",
-    "src/components/model_evaluation.py",
-    "src/pipeline/__init__.py",
-    "src/pipeline/training_pipeline.py",
-    "src/pipeline/prediction_piepline.py",
-    "src/utils/__init__.py",
-    "src/utils/utils.py",
-    "src/logger/logging.py",
-    "src/exception/exception.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
-    "requirements.txt", #for the production
-    "requirements_dev.txt", #for the development
-    "setup.py",
-    "setup.cfg",
-    "pyproject.toml",
-    "tox.ini", #to test in diff diff cases on a local enviornment
-    "experiment/experiments.ipynb"
+package_name = "mongodb_connect"
+
+list_of_files = [
+   ".github/workflows/ci.yaml",
+   "src/__init__.py",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/mongo_crud.py", 
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb", 
 ]
 
 for filepath in list_of_files:
@@ -33,11 +25,9 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for filr : {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass # create an empty file
 
-
-
+#its updated
