f1f28d3b2473139a9336f97f5db1ee8a193b90bd
Sadhana Sasikumar <sadhanasasikumar12@gmail.comm>
2024-02-03 20:13:58 +0530

    updatedd

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_requirements(file_path:str)->List[str]:
 
     return requirements
 
-setup(
+ setup(
     name='DimondPricePrediction',
     version='0.0.1',
     author='sadhana sasikumar',
